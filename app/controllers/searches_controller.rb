352f851b6c65eba11e897649718005f727330dcb
onyoo <roberto9m9@gmail.com>
2016-02-28 00:44:15 +0100

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,24 +9,9 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
-    client_id = "CO3LIXJPH1LYAC5OOTLKLJE334NVDIYG24KUFOVEQ22WVYDP"
-    client_secret = "0NNKMRWRYLCKLPSEE3G10I33WV0BTYXEN2JCJ41TVKKWB52Y"
+    foursquare = FoursquareService.new
+    foursquare.foursquare_search('coffee shop', params[:zipcode])
 
-    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-      req.params['client_id'] = client_id
-      req.params['client_secret'] = client_secret
-      req.params['v'] = '20160201'
-      req.params['near'] = params[:zipcode]
-      req.params['query'] = 'coffee shop'
-    end
-
-    body = JSON.parse(@resp.body)
-
-    if @resp.success?
-      @venues = body["response"]["venues"]
-    else
-      @error = body["meta"]["errorDetail"]
-    end
     render 'search'
 
     rescue Faraday::TimeoutError
--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -20,4 +20,42 @@ class FoursquareService
     end
     JSON.parse(resp.body)['response']['friends']['items']
   end
+
+  def foursquare_search(searching_for, zipcode)
+    client_id = "CO3LIXJPH1LYAC5OOTLKLJE334NVDIYG24KUFOVEQ22WVYDP"
+    client_secret = "0NNKMRWRYLCKLPSEE3G10I33WV0BTYXEN2JCJ41TVKKWB52Y"
+
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
+      req.params['v'] = '20160201'
+      req.params['near'] = zipcode
+      req.params['query'] = searching_for
+    end
+
+    body = JSON.parse(@resp.body)
+
+    if @resp.success?
+      @venues = body["response"]["venues"]
+    else
+      @error = body["meta"]["errorDetail"]
+    end
+  end
+
+  def my_tips(token)
+    resp = Faraday.get("https://api.foursquare.com/v2/lists/self/tips") do |req|
+      req.params['oauth_token'] = token
+      req.params['v'] = '20160201'
+    end
+    @results = JSON.parse(resp.body)["response"]["list"]["listItems"]["items"]
+  end
+
+  def create_tip(token, venue_id, tip)
+    resp = Faraday.post("https://api.foursquare.com/v2/tips/add") do |req|
+      req.params['oauth_token'] = token
+      req.params['v'] = '20160201'
+      req.params['venueId'] = venue_id
+      req.params['text'] = tip
+    end
+  end
 end
