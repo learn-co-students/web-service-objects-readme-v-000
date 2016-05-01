aaf983032988142cc0376cbda294b288f54cd5c6
jsgoldb <jgoldb@yahoo.com>
2016-05-01 16:22:38 -0500

    Done.

---
+++ app/services/foursquare_service.rb
@@ -0,0 +1,54 @@
+class FoursquareService
+
+  def authenticate!(client_id, client_secret, code)
+    resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
+      req.params['grant_type'] = 'authorization_code'
+      req.params['redirect_uri'] = "http://localhost:3000/auth"
+      req.params['code'] = code
+    end
+    body = JSON.parse(resp.body)
+    body["access_token"]
+  end
+
+  def friends(token)
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+    req.params['oauth_token'] = token
+    # don't forget that pesky v param for versioning
+    req.params['v'] = '20160201'
+    end
+    JSON.parse(resp.body)["response"]["friends"]["items"]
+  end
+
+  def list_my_tips(token)
+      resp = Faraday.get("https://api.foursquare.com/v2/lists/self/tips") do |req|
+      req.params['oauth_token'] = token
+      req.params['v'] = '20160201'
+    end
+    tips = JSON.parse(resp.body)["response"]["list"]["listItems"]["items"]
+  end
+
+  def create_tip(token, venue_id, tip)
+    resp = Faraday.post("https://api.foursquare.com/v2/tips/add") do |req|
+    req.params['oauth_token'] = session[:token]
+    req.params['v'] = '20160201'
+    req.params['venueId'] = params[:venue_id]
+    req.params['text'] = params[:tip]
+    end
+  end
+
+  def venues(zipcode)
+    client_id = "CO3LIXJPH1LYAC5OOTLKLJE334NVDIYG24KUFOVEQ22WVYDP"
+    client_secret = "0NNKMRWRYLCKLPSEE3G10I33WV0BTYXEN2JCJ41TVKKWB52Y"
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
+      req.params['v'] = '20160201'
+      req.params['near'] = zipcode
+      req.params['query'] = 'coffee shop'
+    end
+    return @resp
+  end
+
+end
