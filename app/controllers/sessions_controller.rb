0bfee2b0cce005caa1c0d2bde1190acc36123b46
Brad1974 <danieltbrad@gmail.com>
2016-05-24 15:36:31 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,5 @@ class SessionsController < ApplicationController
     session[:token] = foursquare.authenticate!(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'], params[:code])
     redirect_to root_path
   end
+  
 end
