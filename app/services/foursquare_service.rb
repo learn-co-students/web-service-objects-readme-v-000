aacbe44708a0cdadfd3eaeccf9a59d99c415ba21
evanscloud <dijkstrascloud@gmail.com>
2016-05-26 22:54:07 -0400

    Add search api to service object

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -12,9 +12,34 @@ class FoursquareService
   end
 
   def friends(token)
-  resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
-    req.params['oauth_token'] = token
-    req.params['v'] = '20160201'
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = token
+      req.params['v'] = '20160201'
+    end
+    JSON.parse(resp.body)["response"]["friends"]["items"]
   end
-  JSON.parse(resp.body)["response"]["friends"]["items"]
+
+  def search(client_id, client_secret, near, query)
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
+      req.params['v'] = '20160201'
+      req.params['near'] = params[:zipcode]
+      req.params['query'] = 'coffee shop'
+    end
+
+    body = JSON.parse(@resp.body)
+
+    if @resp.success?
+      @venues = body["response"]["venues"]
+    else
+      @error = body["meta"]["errorDetail"]
+    end
+    render 'search'
+
+    rescue Faraday::TimeoutError
+      @error = "There was a timeout. Please try again."
+      render 'search'
+    end
+  end
 end
