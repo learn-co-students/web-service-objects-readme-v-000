679ef509a5602b3dc4ad9541dbeb285038887a40
agdavid <antonio.g.david@gmail.com>
2016-06-08 17:58:12 -0400

    refactor other service objects

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -20,4 +20,31 @@ class FoursquareService
     end
     JSON.parse(resp.body)["response"]["friends"]["items"]
   end
+
+  def foursquare_query(client_id, client_secret, zipcode)
+    Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
+      req.params['v'] = '20160201'
+      req.params['near'] = zipcode
+      req.params['query'] = 'coffee shop'
+    end 
+  end
+
+  def tips_query(token)
+    resp = Faraday.get("https://api.foursquare.com/v2/lists/self/tips") do |req|
+      req.params['oauth_token'] = token
+      req.params['v'] = '20160201'
+    end
+    JSON.parse(resp.body)["response"]["list"]["listItems"]["items"]
+  end
+
+  def tips_create(token, venue_id, tip)
+    resp = Faraday.post("https://api.foursquare.com/v2/tips/add") do |req|
+      req.params['oauth_token'] = token
+      req.params['v'] = '20160201'
+      req.params['venueId'] = venue_id
+      req.params['text'] = tip
+    end
+  end
 end
