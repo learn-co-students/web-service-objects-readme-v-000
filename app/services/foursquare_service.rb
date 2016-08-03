42127caf3a75cebeba94790ebdce7bc37b1e7691
James Novak <jamesnovak90@gmail.com>
2016-08-02 21:10:46 -0400

    Done.

---
+++ app/services/foursquare_service.rb
@@ -0,0 +1,25 @@
+class FoursquareService
+
+  def authenticate!
+    resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
+      req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']
+      req.params['client_secret'] = ENV['FOURSQUARE_SECRET']
+      req.params['grant_type'] = 'authorization_code'
+      req.params['redirect_uri'] = "http://localhost:3000/auth"
+      req.params['code'] = params[:code]
+    end
+      body = JSON.parse(resp.body)
+      body["access_token"]
+  end
+
+
+  def friends(token)
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = session[:token]
+      # don't forget that pesky v param for versioning
+      req.params['v'] = '20160201'
+    end
+    @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
+  end
+  
+end
