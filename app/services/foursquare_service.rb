65e50b0a9dcc1b4d6c1c4d6184cc590d6db895fc
kade042 <dereje88@gmail.com>
2016-10-25 18:02:55 -0600

    Done.

---
+++ app/services/foursquare_service.rb
@@ -0,0 +1,67 @@
+class FoursquareService
+
+  def authenticate!(client_id, client_secret, code)
+    resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
+      req.params['grant_type'] = 'authorization_code'
+      req.params['redirect_uri'] = "http://localhost:3000/auth"
+      req.params['code'] = code
+    end
+    body = JSON.parse(resp.body)
+    body["access_token"]
+  end
+
+  def friends(token)
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = token
+      req.params['v'] = '20160201'
+    end
+    JSON.parse(resp.body)["response"]["friends"]["items"]
+  end
+
+  def foursquare(zipcode, client_id, client_secret)
+      @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+        req.params['client_id'] = client_id
+        req.params['client_secret'] = client_secret
+        req.params['v'] = '20160201'
+        req.params['near'] = zipcode
+        req.params['query'] = 'coffee shop'
+      end
+
+      body = JSON.parse(@resp.body)
+
+      begin
+        if @resp.success?
+          @venues = body["response"]["venues"]
+        else
+          @error = body["meta"]["errorDetail"]
+        end
+
+      rescue Faraday::TimeoutError
+        @error = "There was a timeout. Please try again."
+      end
+
+
+  end
+
+  def list_tips(token)
+    resp = Faraday.get("https://api.foursquare.com/v2/lists/self/tips") do |req|
+      req.params['oauth_token'] = token
+      req.params['v'] = '20160201'
+    end
+     JSON.parse(resp.body)["response"]["list"]["listItems"]["items"]
+
+  end
+
+  def add_tips(token, venues_id, tip)
+    resp = Faraday.post("https://api.foursquare.com/v2/tips/add") do |req|
+      req.params['oauth_token'] = token
+      req.params['v'] = '20160201'
+      req.params['venueId'] = venues_id
+      req.params['text'] = tip
+    end
+
+  end
+
+end
