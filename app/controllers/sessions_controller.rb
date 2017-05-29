ba3a85d3e3d7dcf3a211e9e1d9932c66dea60873
bentonwong <bentonwong@gmail.com>
2017-05-28 18:01:37 -0700

    done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,8 +10,8 @@ class SessionsController < ApplicationController
       req.params['code'] = params[:code]
     end
 
-    body = JSON.parse(resp.body)
-    session[:token] = body["access_token"]
+    foursquare = FoursquareService.new
+    session[:token] = foursquare.authenticate!(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'], params[:code])
     redirect_to root_path
   end
 end
