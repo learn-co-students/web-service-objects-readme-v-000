cef66bdcf074629fc11c70b94695b04962b5eab9
James Fernandez <jffernan@comcast.net>
2017-08-29 10:41:42 -0700

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,10 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
+  def create
+  foursquare = FoursquareService.new
+  session[:token] = foursquare.authenticate!(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'], params[:code])
+  redirect_to root_path
+  end
+  
   end
