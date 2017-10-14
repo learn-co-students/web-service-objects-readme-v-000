4cc766fc078bac98e070cf3427b2d9964b3c3460
Matteo Piccini <gnappo.1@live.it>
2017-10-14 04:38:39 +0000

    Done.

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -19,4 +19,39 @@ class FoursquareService
     end
     JSON.parse(resp.body)["response"]["friends"]["items"]
   end
+
+  def venues(client_id, client_secret, zipcode)
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
+      req.params['v'] = '20160201'
+      req.params['near'] = zipcode
+      req.params['query'] = 'coffee shop'
+    end
+
+    body = JSON.parse(@resp.body)
+
+    if @resp.success?
+      body["response"]["venues"]
+    else
+      @error = body["meta"]["errorDetail"]
+    end
+  end
+
+  def tips_index(token)
+		resp = Faraday.get("https://api.foursquare.com/v2/lists/self/tips") do |req|
+	      req.params['oauth_token'] = token
+	      req.params['v'] = '20160201'
+	    end
+	    JSON.parse(resp.body)["response"]["list"]["listItems"]["items"]
+	end
+
+	def create_tip(token, venue_id, tip)
+		resp = Faraday.post("https://api.foursquare.com/v2/tips/add") do |req|
+	    req.params['oauth_token'] = token
+	    req.params['v'] = '20160201'
+	    req.params['venueId'] = venue_id
+	    req.params['text'] = tip
+	  end
+	end
 end
