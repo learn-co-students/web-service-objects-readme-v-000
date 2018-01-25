b7f5475c1eaca294c7341266c79e9d9858e3aff3
emorgan05 <emorgan05@gmail.com>
2018-01-25 16:48:48 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'searches#search'
   get '/search', to: 'searches#search'
-  post '/search', to: 'searches#foursquare'
+  post '/search', to: 'searches#venue_list'
 
   get '/auth', to: 'sessions#create'
   get '/friends', to: 'searches#friends'
