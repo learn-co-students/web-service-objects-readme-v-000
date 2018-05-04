c489ab053c8460bf1946783256d2141ee5af71fe
terryblue99 <terryblue@comcast.net>
2018-05-04 17:06:12 -0500

    Done.

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -15,9 +15,31 @@ class FoursquareService
 
 	end
 
+	def venues(zipcode)
+
+		resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+	      req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']
+	      req.params['client_secret'] = ENV['FOURSQUARE_SECRET']
+	      # don't forget that pesky v param for versioning
+	      req.params['v'] = '20160201'
+	      req.params['near'] = zipcode
+	      req.params['query'] = 'coffee shop'
+	    end
+
+   		body = JSON.parse(resp.body)
+
+   		if resp.success?
+	      body["response"]["venues"]
+	    else
+	      body["meta"]["errorDetail"]
+	    end
+
+	end
+
 	def friends(token)
 	    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
 	      req.params['oauth_token'] = token
+	      # don't forget that pesky v param for versioning
 	      req.params['v'] = '20160201'
 	    end
 	    JSON.parse(resp.body)["response"]["friends"]["items"]
