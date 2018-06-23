bade77de47fbf30c487082fd732ade7c1dc51261
jennianelson <jennifernelson118@gmail.com>
2018-06-23 17:04:14 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -36,7 +36,8 @@ gem 'dotenv-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
+  # gem 'byebug'
+  gem 'pry'
 end
 
 group :development do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 private
 
   def authenticate_user
-    client_id = ENV['FOURSQUARE_CLIENT_ID']
+    client_id = ENV['CLIENT_ID']
     redirect_uri = CGI.escape("http://localhost:3000/auth")
     foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
     redirect_to foursquare_url unless logged_in?
