97208c8ef6d8f25adf8aaa34b24c14c444eda685
heislercreative <brennan@heislercreative.com>
2018-06-30 18:57:26 -0600

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -12,20 +12,13 @@ class SearchesController < ApplicationController
     client_id = "CO3LIXJPH1LYAC5OOTLKLJE334NVDIYG24KUFOVEQ22WVYDP"
     client_secret = "0NNKMRWRYLCKLPSEE3G10I33WV0BTYXEN2JCJ41TVKKWB52Y"
 
-    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-      req.params['client_id'] = client_id
-      req.params['client_secret'] = client_secret
-      req.params['v'] = '20160201'
-      req.params['near'] = params[:zipcode]
-      req.params['query'] = 'coffee shop'
-    end
+    foursquare = FoursquareService.new
+    @results = foursquare.search(client_id, client_secret, params[:zipcode])
 
-    body = JSON.parse(@resp.body)
-
-    if @resp.success?
-      @venues = body["response"]["venues"]
+    if @results.success?
+      @venues = @results["response"]["venues"]
     else
-      @error = body["meta"]["errorDetail"]
+      @error = @results["meta"]["errorDetail"]
     end
     render 'search'
 
--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -21,4 +21,16 @@ class FoursquareService
     JSON.parse(resp.body)["response"]["friends"]["items"]
   end
 
+  def search(client_id, client_secret, zipcode)
+    resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
+      req.params['v'] = '20160201'
+      req.params['near'] = params[:zipcode]
+      req.params['query'] = 'coffee shop'
+    end
+
+    JSON.parse(resp.body)
+  end
+
 end
