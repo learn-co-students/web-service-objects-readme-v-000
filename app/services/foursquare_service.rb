8a50c1ce8db8341b9b387b276b4bb733f18265e4
Sean Oughton <seanoughton@gmail.com>
2018-07-12 16:29:21 +0000

    Done.

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -12,4 +12,13 @@ class FoursquareService
     body = JSON.parse(resp.body)
     body["access_token"]
   end
+
+
+  def friends(token)
+      resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = token
+      req.params['v'] = '20160201'
+      end
+      JSON.parse(resp.body)["response"]["friends"]["items"]
+  end
 end
