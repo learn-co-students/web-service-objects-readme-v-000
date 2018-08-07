98a37968d7e482e8dad3d33ee6e613012c0a0d7a
anthonygharvey <anthony@anthonygharvey.com>
2018-08-06 20:25:40 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'searches#search'
   get '/search', to: 'searches#search'
   post '/search', to: 'searches#foursquare'
+  get '/logout', to: 'sessions#logout'
 
   get '/auth', to: 'sessions#create'
   get '/friends', to: 'searches#friends'
