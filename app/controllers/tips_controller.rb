cda1bf963a34a5170a0e4260bc89e350bd05c3a9
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-08-09 22:16:49 -0400

    Done.

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -9,10 +9,12 @@ class TipsController < ApplicationController
 
   def create
     resp = Faraday.post("https://api.foursquare.com/v2/tips/add") do |req|
-      req.params['oauth_token'] = session[:token]
-      req.params['v'] = '20160201'
-      req.params['venueId'] = params[:venue_id]
-      req.params['text'] = params[:tip]
+      req.body = {
+        'oauth_token': session[:token],
+        'v': '20160201',
+        'venueId': params[:venue_id],
+        'text': params[:tip]
+      }
     end
 
     redirect_to tips_path
