67f4681df5f81e822cfc25eda9d995c3be3ed1a0
Vanessa Toro <vanetoro84@gmail.com>
2018-08-15 14:18:11 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,9 @@ class SessionsController < ApplicationController
 
   def create
     foursquare = FoursquareService.new
+    binding.pry
     session[:token] = foursquare.authenticate!(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'], params[:code])
     redirect_to root_path
   end
-  
+
 end
--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -6,10 +6,32 @@ class FoursquareService
       req.params['client_id'] = client_id
       req.params['client_secret'] = client_secret
       req.params['grant_type'] = 'authorization_code'
-      req.params['redirect_uri'] = "http://67.205.182.198:55372/auth"
+      req.params['redirect_uri'] = "http://localhost:3000/auth"
       req.params['code'] = code
+      binding.pry
     end
     body = JSON.parse(resp.body)
-    session[:token] = body["access_token"]
+    binding.pry
+    body["access_token"]
+  end
 
+  def friends(token)
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = token
+      # don't forget that pesky v param for versioning
+      req.params['v'] = '20160201'
+    end
+    JSON.parse(resp.body)["response"]["friends"]["items"]
+  end
+
+  def foursquare(client_id, client_secret, zipcode)
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
+      req.params['v'] = '20160201'
+      req.params['near'] = zipcode
+      req.params['query'] = 'coffee shop'
+    end
+  end
+  
 end
