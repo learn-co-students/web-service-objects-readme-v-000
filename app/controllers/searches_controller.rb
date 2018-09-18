8378a391f9ddc0facb1a87e8198ab69817972c3e
Emi Ko <emi8emi@gmail.com>
2018-09-18 13:09:38 -0700

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,15 +3,16 @@ class SearchesController < ApplicationController
   def search
   end
 
-  
+  def friends
+   foursquare = FoursquareService.new
+   @friends = foursquare.friends(session[:token])
+  end
+
 
   def foursquare
-    client_id = "CO3LIXJPH1LYAC5OOTLKLJE334NVDIYG24KUFOVEQ22WVYDP"
-    client_secret = "0NNKMRWRYLCKLPSEE3G10I33WV0BTYXEN2JCJ41TVKKWB52Y"
-
     @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-      req.params['client_id'] = client_id
-      req.params['client_secret'] = client_secret
+      req.params['client_id'] = ENV['CLIENT_ID']
+      req.params['client_secret'] = ENV['CLIENT_SECRET']
       req.params['v'] = '20160201'
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
