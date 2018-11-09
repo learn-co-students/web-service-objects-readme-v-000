c6baf2a891ab9db90f9c700dae2aae1c3ae24c1e
github username <github email address>
2018-11-08 22:40:28 -0800

    Refactor #foursquare method in searches_controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,23 +8,12 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
-    client_id = "CO3LIXJPH1LYAC5OOTLKLJE334NVDIYG24KUFOVEQ22WVYDP"
-    client_secret = "0NNKMRWRYLCKLPSEE3G10I33WV0BTYXEN2JCJ41TVKKWB52Y"
+    result = FoursquareService.new.foursquare(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_CLIENT_SECRET'], params[:zipcode])
 
-    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-      req.params['client_id'] = client_id
-      req.params['client_secret'] = client_secret
-      req.params['v'] = '20160201'
-      req.params['near'] = params[:zipcode]
-      req.params['query'] = 'coffee shop'
-    end
-
-    body = JSON.parse(@resp.body)
-
-    if @resp.success?
-      @venues = body["response"]["venues"]
+    if response.successful?
+      @venues = result["response"]["venues"]
     else
-      @error = body["meta"]["errorDetail"]
+      @error = result["meta"]["errorDetail"]
     end
     render 'search'
 
