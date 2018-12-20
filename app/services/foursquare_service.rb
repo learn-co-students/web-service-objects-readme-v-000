906b78df91437b14a9abb068557db6bbbdf03f54
roitblatari <roitblatari@gmail.com>
2018-12-20 13:45:45 -0500

    Done.

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -1,22 +1,22 @@
 class FoursquareService
 
-  def foursquare_it(client_id, client_secret, zipcode)
-# this does not work
-    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-      req.params['client_id'] = client_id
-      req.params['client_secret'] = client_secret
-      req.params['v'] = '20160201'
-      req.params['near'] = zipcode
-      req.params['query'] = 'coffee shop'
-    end
-    body = JSON.parse(@resp.body)
+#   def foursquare_it(client_id, client_secret, zipcode)
+# # this does not work
+#     @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+#       req.params['client_id'] = client_id
+#       req.params['client_secret'] = client_secret
+#       req.params['v'] = '20160201'
+#       req.params['near'] = zipcode
+#       req.params['query'] = 'coffee shop'
+#     end
+#     body = JSON.parse(@resp.body)
 
-    if @resp.success?
-      @venues = body["response"]["venues"]
-    else
-      @error = body["meta"]["errorDetail"]
-    end
-  end
+#     if @resp.success?
+#       @venues = body["response"]["venues"]
+#     else
+#       @error = body["meta"]["errorDetail"]
+#     end
+#   end
 
   def authenticate!(client_id, client_secret, code)
     resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
