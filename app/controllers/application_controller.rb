91db7d9bfa9a8533a11904dfbe22d5a93c150b85
github username <github email address>
2019-05-14 22:31:49 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,10 @@ class ApplicationController < ActionController::Base
 
 private
 
-  def authenticate_user
-    client_id = ENV['FOURSQUARE_CLIENT_ID']
-    redirect_uri = CGI.escape("http://localhost:3000/auth")
-    foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
-    redirect_to foursquare_url unless logged_in?
+  def create
+    foursquare = FoursquareService.new
+    session[:token] = foursquare.authenticate!(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'], params[:code])
+    redirect_to root_path
   end
 
   def logged_in?
