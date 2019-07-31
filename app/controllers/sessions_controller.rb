29d6ee026f968ca48760fd05737baaf8758dc45a
github username <github email address>
2019-07-31 10:12:43 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
+    #binding.pry
     foursquare = FoursquareService.new
     session[:token] = foursquare.authenticate!(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'], params[:code])
     redirect_to root_path
--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -1,6 +1,7 @@
 class FoursquareService
 
   def authenticate!(client_id, client_secret, code)
+    binding.pry
     resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
       req.params['client_id'] = client_id
       req.params['client_secret'] = client_secret
@@ -10,6 +11,14 @@ class FoursquareService
     end
     body = JSON.parse(resp.body)
     body["access_token"]
+  end
+
+  def friends(token)
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = token
+      req.params['v'] = '20160201'
+    end
+    JSON.parse(resp.body)["response"]["friends"]["items"]
   end
 
 end
